ece07bc60f6d13ba7fe39b8a7aca322adafe8036
agis22 <up1072803@upnet.gr>
2023-03-01 01:01:18 +0200

    updated lab1.py

--- lab1.py
+++ lab1.py
@@ -1,7 +1,18 @@
 import requests  # εισαγωγή της βιβλιοθήκης
 
-url = input("Give URL:")  # προσδιορισμός του url
+def print_item(iter):
+    for key,value in iter:
+        print(f"{key}")
+
+url = input("Give URL: ")  # προσδιορισμός του url
 
 with requests.get(url) as response:  # το αντικείμενο response
     headers = response.headers
-    print(headers)
+    cookies = response.cookies
+    print(f"\nHeaders:")  
+    print_item(headers.items())     # τυπώνει όλες τις κεφαλίδες
+    print(f"\nServer: {headers['server']}\n")  # τυπώνει το λογισμικό του server
+    
+    #if cookies != None:
+    print(headers['set-cookie']['expires'])
+    #print_item(cookies.items())  # τυπώνει το λογισμικό του server
